91ce96d26cad7c046f82de33617e34e943ffad2e
Viktor <viktor.rachynskyi@asquad.io>
2022-03-15 19:55:33 +0200

    refactoring test class

--- src/test/java/com/github/SelenideTest.java
+++ src/test/java/com/github/SelenideTest.java
@@ -4,21 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
 
     @Test
-    void test (){
+    void searchJUnit5CodeBlockTest() {
         open("https://github.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $(".repo-list a").click();
         $("#wiki-tab").click();
         $(byText("Soft assertions")).click();
         $("#user-content-3-using-junit5-extend-test-class").parent()
-                        .shouldHave(text("Using JUnit5 extend test class:"))  // search JUnit5 section
-                        .sibling(0).$("div pre").shouldBe(visible); // search visible code block
+                .shouldHave(text("Using JUnit5 extend test class:"))  // search JUnit5 section
+                .sibling(0).$("div pre").shouldBe(visible); // search visible code block
     }
-
 }
