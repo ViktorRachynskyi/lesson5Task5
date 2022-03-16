bf5ee12c90d78670131d0f24ec5ebca7ee33a779
Viktor <viktor.rachynskyi@asquad.io>
2022-03-16 16:05:23 +0200

    edit test

--- src/test/java/com/github/SelenideTest.java
+++ src/test/java/com/github/SelenideTest.java
@@ -14,7 +14,6 @@ public class SelenideTest {
         $("#wiki-tab").click();
         $("#wiki-pages-filter").setValue("SoftAssertion");
         $(byText("SoftAssertions")).click();
-        sleep(3000);
         $("#user-content-3-using-junit5-extend-test-class").parent()
                 .shouldHave(text("Using JUnit5 extend test class:"))  // search JUnit5 section
                 .sibling(0).$("div pre").shouldBe(visible); // search visible code block
